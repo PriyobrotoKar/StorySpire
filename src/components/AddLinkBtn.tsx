10c78fb8b42aa7b9396511e133fec09a0ecd2539
Priyobroto Kar <priyobrotokar@gmail.com>
2023-11-12 20:57:04 +0530

    feat:add social model schema

--- src/components/AddLinkBtn.tsx
+++ src/components/AddLinkBtn.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { MdAdd } from "react-icons/md";
+import { Input } from "./ui/input";
 
 const AddLinkBtn = () => {
   return (
     <Button variant={"secondary"} className="gap-2 pl-2 pr-4">
-      <MdAdd className="h-7 w-7 rounded-sm bg-slate-300 p-1 text-lg text-slate-500" />
+      <div className="rounded-[6px] bg-slate-300 p-1">
+        <MdAdd className={"text-slate-500"} />
+      </div>
+      <Input
+        className="border-none bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
+        type="text"
+        placeholder="Type your link address"
+      />
       Add
     </Button>
   );
